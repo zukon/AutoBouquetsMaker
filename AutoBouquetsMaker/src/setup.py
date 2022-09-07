bdcd35afc5391fc9136c093c685cccc09e167929
Azureit <me@localhost>
2022-09-07 23:35:38 +0100

    Fix region sort with accents

--- AutoBouquetsMaker/src/setup.py
+++ AutoBouquetsMaker/src/setup.py
@@ -20,6 +20,7 @@ from .scanner.providerconfig import ProviderConfig
 from . import log
 import itertools
 import six
+import unicodedata
 
 
 class AutoBouquetsMaker_ProvidersSetup(ConfigListScreen, Screen):
@@ -246,7 +247,7 @@ class AutoBouquetsMaker_ProvidersSetup(ConfigListScreen, Screen):
 			bouquets = self.providers[provider]["bouquets"]
 			for bouquet in list(bouquets.keys()):
 				arealist.append((bouquet, self.providers[provider]["bouquets"][bouquet]["name"]))
-			arealist.sort(key=lambda x: x[1])
+			arealist.sort(key=lambda x: unicodedata.normalize('NFKD',x[1].upper()).encode('ascii', 'ignore').decode('utf-8'))
 			if len(self.providers[provider]["bouquets"]) > 0: # provider has area list
 				default_area = None
 				if provider in providers_tmp_configs:
